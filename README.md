4cb4f47cb8730faee803718ccb080676a0081dc3
Jithin Lal K <jithinlal@qburst.com>
2019-08-16 12:21:52 +0530

    added files

---
+++ README.md
@@ -0,0 +1,6 @@
+# Analog Clock
+**Screenshot**   
+![](Images/Screenshot.png)   
+**Visit website**
+* http://10.4.6.98:8000 (Hosted on local server)   
+* https://jithinlalk.github.io (Hosted on GitHub)
--- script.js
+++ script.js
@@ -1,177 +1,212 @@
-const CANVAS_WIDTH = 500;
-const CANVAS_HEIGHT = 500;
-const CANVAS_CENTER_X = CANVAS_WIDTH/2;
-const CANVAS_CENTER_Y = CANVAS_HEIGHT/2;
-const RADIUS = 200;
-const PI = Math.PI;
-const ALARM_AUDIO_LENGTH = 29;
-
 var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-// Counter for hour bell and alarm
+var ctx = canvas.getContext("2d");
 var hourBellCounter = 0;
 var alarmCounter = 0;
-// Initialize audio objects
-var audioTick = new Audio("tick.mp3");
-var audioBell = new Audio("bell.mp3");
-var audioAlarm = new Audio("alarm.mp3");
-// Array for storing list of alarms
-var alarmList = [];
-// Checkbox for mute/unmute
+var audioTick = new Audio("Audios/tick.mp3");
+var audioBell = new Audio("Audios/bell.mp3");
+var audioAlarm = new Audio("Audios/alarm.mp3");
+var alarm_audio_length = 29;
 var soundCheckbox = document.getElementById("sound");
+var radius;
 
-// Change canvas origin to center
-context.translate(CANVAS_CENTER_X, CANVAS_CENTER_Y);
-
-// Set clock frame
 var background = new Image();
-background.src = "clock_frame.png";
+background.src = "Images/clock_frame.png";
 
-// Run funtion 'drawClock' in every second
-setInterval(function drawClock() {
-    drawFrame();
-    drawAllHands();
-}, 1000);
+// Switch between alarm gif and canvas
+function displayAlarmCanvas(alarmStyle, canvasStyle) {
+  document.getElementById("alarmImage").style.display = alarmStyle;
+  document.getElementById("canvasDiv").style.display = canvasStyle;
+}
+
+function Orientation(orientLandscape) {
+  if (orientLandscape.matches) {
+    // If media query matches
+    canvas.width = "250";
+    canvas.height = "250";
+  } else {
+    canvas.width = "150";
+    canvas.height = "150";
+  }
+  radius = canvas.height / 2;
+  ctx.translate(radius, radius);
+  radius = radius * 0.9;
+}
+
+// Set canvas size corresponding to orientation
+var orientLandscape = window.matchMedia("(orientation: landscape)");
+Orientation(orientLandscape);
+orientLandscape.addListener(Orientation);
+
+// Get list of alarms from cookie and display
+var alarmList = JSON.parse((document.cookie === "") ? "[]" : document.cookie);
+alarmList.forEach(function Alarm(time) {
+  var li = document.createElement("LI");
+  li.innerHTML = time + '<span onclick="deleteAlarm(this)"' + 'class="close">&times;</span>';
+  document.getElementById("alarms").appendChild(li);
+});
 
 // Draw clock frame
 function drawFrame() {
-    // Set white background
-    context.beginPath();
-    context.arc(0, 0, RADIUS, 0, 2*PI);
-    context.fillStyle = "white";
-    context.fill();
-
-    // Set background image
-    context.drawImage(background, 0-CANVAS_CENTER_X, 0-CANVAS_CENTER_Y,
-                      CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2 * 3.14);
+  ctx.fillStyle = "white";
+  ctx.fill();
+  ctx.drawImage(background, 0 - canvas.width / 2, 0 - canvas.height / 2, canvas.width, canvas.height);
 }
 
-// Calculate time and draw all hands
-function drawAllHands() {
-    var date = new Date();
-    var hour = date.getHours();
-    var minute = date.getMinutes();
-    var second = date.getSeconds();
+// Draw single hand
+function drawHand(ctx, pos, length, width) {
+  ctx.beginPath();
+  ctx.lineWidth = width;
+  ctx.lineCap = "round";
+  ctx.moveTo(0, 0);
+  ctx.rotate(pos);
+  ctx.lineTo(0, -length);
+  ctx.stroke();
+  ctx.rotate(-pos);
+}
 
-    // Update hourBellCounter in every hour
-    if(0===minute && 0===second) {
-        hourBellCounter = ((hour-1)%12)+1;
+// Draw all hands
+function drawTime(ctx, radius) {
+  var now = new Date();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
+
+  // Update hourBellCounter in every hour
+  if (0 === minute && 0 === second) {
+    hourBellCounter = ((hour - 1) % 12) + 1;
+  }
+
+  // Check if any alarm is set in every minute
+  if (0 === second) {
+    alarmList.forEach(function checkAlarm(item) {
+      var time = item.split(":");
+      if (parseInt(time[0]) === hour && parseInt(time[1]) === minute) {
+        audioAlarm.play();
+        alarmCounter = alarm_audio_length;
+      }
+    });
+  }
+
+  // Display date and time
+  document.getElementById("time").innerHTML = now;
+
+  // Convert hour from 24-hour format to 12-hour format
+  hour = hour % 12;
+  hour = (hour * Math.PI) / 6 + (minute * Math.PI) / (6 * 60) + (second * Math.PI) / (360 * 60);
+  drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+
+  //minute
+  minute = (minute * Math.PI) / 30 + (second * Math.PI) / (30 * 60);
+  drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+
+  // second
+  second = (second * Math.PI) / 30;
+  drawHand(ctx, second, radius * 0.9, radius * 0.02);
+
+  // Check if muted or not
+  if (soundCheckbox.checked) {
+    if (alarmCounter > 0) {
+      // If alarm is playing hide canvas and display alarm gif
+      displayAlarmCanvas("block", "none");
+      alarmCounter--;
+    } else if (hourBellCounter > 0) {
+      displayAlarmCanvas("none", "block");
+      // Play hour bell
+      audioBell.play();
+      hourBellCounter--;
+    } else {
+      displayAlarmCanvas("none", "block");
+      // Play tick
+      audioTick.play();
     }
-
-    // Check if any alarm is set in every minute
-    if(0===second) {
-        alarmList.forEach(function checkAlarm(item) {
-            var time = item.split(":");
-            if(parseInt(time[0])===hour && parseInt(time[1])===minute){
-                audioAlarm.play();
-                alarmCounter = ALARM_AUDIO_LENGTH;
-            }
-        });
-    }
-
-
-    // Display date and time
-    document.getElementById("time").innerHTML = date;
-
-    // Draw hour hand
-    hour = hour%12;
-    hour = (hour*PI/6) + (minute*PI/360) + (second*PI/21600);
-    drawHand("black", hour, RADIUS*0.5, RADIUS*0.07);
-
-    // Draw minute hand
-    minute = (minute*PI/30) + (second*PI/1800);
-    drawHand("black", minute, RADIUS*0.8, RADIUS*0.07);
-
-    // Draw second hand
-    second = (second*PI/30);
-    drawHand("red", second, RADIUS*0.9, RADIUS*0.02);
-
-    // Check if muted or not
-    if(soundCheckbox.checked) {
-        if(alarmCounter>0) {
-            // If alarm is playing hide canvas and display alarm gif
-            displayAlarmCanvas("block", "none");
-            alarmCounter--;
-        }else if(hourBellCounter>0) {
-            displayAlarmCanvas("none", "block");
-            // Play hour bell
-            audioBell.play();
-            hourBellCounter--;
-        }else {
-            displayAlarmCanvas("none", "block");
-            audioTick.play();
-        }
-    }else {
-        displayAlarmCanvas("none", "block");
+  } else {
+    displayAlarmCanvas("none", "block");
 
     // If muted stop alarm and reset hourBellCounter and alarmCounter
     audioAlarm.pause();
     audioAlarm.currentTime = 0;
     hourBellCounter = 0;
     alarmCounter = 0;
-    }
-}
-
-// Switch between alarm gif and canvas
-function displayAlarmCanvas(alarmStyle, canvasStyle) {
-    document.getElementById("alarmImage").style.display = alarmStyle;
-    document.getElementById("canvas").style.display = canvasStyle;
-}
-
-// Draw hand
-function drawHand(color, angle, length, width) {
-    context.beginPath();
-    context.lineWidth = width;
-    context.lineCap = "round";
-    context.moveTo(0, 0);
-    context.rotate(angle);
-    context.lineTo(0, -length);
-    context.strokeStyle = color;
-    context.stroke();
-    context.rotate(-angle);
+  }
 }
 
 // Mute/unmute sound
 function sound() {
-    // Get the status text
-    var text = document.getElementById("text");
+  // Get the status text
+  var text = document.getElementById("text");
 
-    // If the checkbox is checked display the status text
-    if(soundCheckbox.checked) {
-        text.innerHTML = "ON";
-    }else {
-        text.innerHTML = "OFF";
-    }
+  // Change the status text
+  if (soundCheckbox.checked) {
+    text.innerHTML = "ON";
+  } else {
+    text.innerHTML = "OFF";
+  }
 }
 
-// Add new alarm
-function addAlarm() {
-    var time = document.getElementById("alarmTime").value;
-    if(time === ""){
-        return;
-    }
-
-    // Add new element to HTML alarm list
-    var li = document.createElement("LI");
-    li.innerHTML = time+" "+"<span onclick=\"deleteAlarm(this)\""+
-                   "class=\"close\">&times;</span>";
-    document.getElementById("alarms").appendChild(li);
-	document.getElementById("alarmTime").value = "";
-    updateAlarmList();
-}
-
-// Delete alarm
-function deleteAlarm(t) {
-    t.parentNode.parentNode.removeChild(t.parentNode);
-    updateAlarmList();
+function playSound(play) {
+  document.getElementById("body").style.display = "block";
+  document.getElementById("playSound").style.display = "none";
+  if (play) {
+    soundCheckbox.checked = true;
+    sound();
+  } else {
+    soundCheckbox.checked = false;
+    sound();
+  }
+  setInterval(function drawClock() {
+    drawFrame();
+    drawTime(ctx, radius);
+  }, 1000);
 }
 
 // Update alarm List on adding or deleting alarm
 function updateAlarmList() {
-    alarmList = [];
-    var ul = document.getElementById("alarms");
+  alarmList = [];
+  var ul = document.getElementById("alarms");
 
-    Array.prototype.forEach.call(ul.children, function addToList(child){
-        alarmList.push(child.firstChild.data);
-    });
+  Array.prototype.forEach.call(ul.children, function addToList(child) {
+    alarmList.push(child.firstChild.data);
+  });
+
+  // Add alarm list to cookie
+  var json_str = JSON.stringify(alarmList);
+  document.cookie = json_str;
+}
+
+// Add new alarm
+function addAlarm() {
+  var time = document.getElementById("alarmTime").value;
+
+  alarmList.forEach(function Alarm(ti) {
+    if (time.trim() === ti.trim()) {
+      time = "";
+    }
+  });
+
+  if (time === "") {
+    return;
+  }
+
+  // Add new element to HTML alarm list
+  var li = document.createElement("LI");
+  li.innerHTML =
+    time +
+    " " +
+    '<span onclick="deleteAlarm(this)"' +
+    'class="close">&times;</span>';
+  document.getElementById("alarms").appendChild(li);
+  document.getElementById("alarmTime").value = "";
+  updateAlarmList();
+}
+
+// Delete alarm
+function deleteAlarm(element) {
+
+  if (confirm("Are you sure to delete alarm?")) {
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    updateAlarmList();
+  }
+
 }
